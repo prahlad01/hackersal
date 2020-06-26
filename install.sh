df25eedf91dbd695afc180a05ca208abca93e558
prahlad01 <43715907+prahlad01@users.noreply.github.com>
2020-06-26 16:10:09 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,22 +13,30 @@ echo   "####################################"
 echo "."
 git clone https://github.com/IAmBlackHacker/Facebook-BruteForce
 mv Facebook-BruteForce $HOME
-cd ; ls ; cd Facebook-BruteForce ; chmod +x *
+cd ; ls ; cd Facebook-BruteForce ; chmod +x * ; pip3 installrequests bs4 ; pip install mechanize 
 git  clone https://github.com/JTECHCODE/crackinsta
 mv crackinsta $HOME 
+cd ; cd crackinsta ; chmod +x * ; 
 git clone https://github.com/Mebus/cupp
 mv cupp $HOME
+cd ; cd cupp ; chmod +x * ;
 git clone https://github.com/Gameye98/Lazymux
 mv Lazymux $HOME
+cd ; cd Lazymux ; chmod +x * ;
 git clone https://github.com/rajkumardusad/Tool-X
 mv Tool-X $HOME
+cd ; cd Tool-X ; chmod +x * ; 
 git clone https://github.com/Ranginang67/DarkFly-Tool
 mv DarkFly-Tool $HOME
+cd ; cd DarkFly-Tool ; chmod +x * ;
 git clone https://github.com/noob-hackers/ipdrone
 mv ipdrone $HOME
+cd ; cd ipdrone ; chmod +x * ;
 git clone https://github.com/htr-tech/zphisher
 mv zphisher $HOME
+cd ; cd zphisher ; apt install git curl php openssh -y ; chmod +x * ; 
 git clone https://github.com/websploit/websploit
 mv websploit $HOME
+cd ; cd websploit ; chmod +x * , pip install requirements.txt ; 
 
 exit
