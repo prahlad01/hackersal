094bee569a85ed1ce948b479d6fd6b5bf8b29ddd
prahlad01 <43715907+prahlad01@users.noreply.github.com>
2020-06-26 12:27:51 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo $red 
+ echo ""----------------------------------------------------------------------------- ""
+echo " This Script is written by Prahlad Shukla now start"
+echo "---------------------------------------------------------------------------------"
+echo "."
+
+echo  "#####################################"
+echo   " Learn Free Hacking"               
+ echo   " Subscribe to YouTube Channel The Cyber Kiddie"                                                                    
+echo   "####################################"
+
+echo "."
+git clone https://github.com/IAmBlackHacker/Facebook-BruteForce
+
+exit
