cf65883d899ce80f443678f39bb21197fa61c988
prahlad01 <43715907+prahlad01@users.noreply.github.com>
2020-06-26 15:54:40 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,11 +13,22 @@ echo   "####################################"
 echo "."
 git clone https://github.com/IAmBlackHacker/Facebook-BruteForce
 mv Facebook-BruteForce $HOME
+cd ; ls ; cd Facebook-BruteForce ; chmod +x *
 git  clone https://github.com/JTECHCODE/crackinsta
 mv crackinsta $HOME 
 git clone https://github.com/Mebus/cupp
 mv cupp $HOME
 git clone https://github.com/Gameye98/Lazymux
 mv Lazymux $HOME
+git clone https://github.com/rajkumardusad/Tool-X
+mv Tool-X $HOME
+git clone https://github.com/Ranginang67/DarkFly-Tool
+mv DarkFly-Tool $HOME
+git clone https://github.com/noob-hackers/ipdrone
+mv ipdrone $HOME
+git clone https://github.com/htr-tech/zphisher
+mv zphisher $HOME
+git clone https://github.com/websploit/websploit
+mv websploit $HOME
 
 exit
